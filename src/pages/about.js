d067ef93e78f634d10f4e1371a60138eabe939b1
Yaroslav <doctor355@yahoo.com>
2019-06-07 20:47:42 +0300

    Creating Pages

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Layout from '../components/layout'
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Us</h1>
+        <p>
+          This is about us text... Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Magnam, repellendus. Fugiat, eligendi. Repudiandae
+          sunt vel ipsam minus excepturi nostrum ut in quam eveniet veritatis
+          nesciunt earum laborum, corporis beatae voluptas! Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Vel aspernatur quos expedita
+          nam saepe, officiis ipsam? Dignissimos aperiam quidem iusto voluptas
+          reprehenderit odit rerum? Labore obcaecati illum delectus et nam
+          pariatur recusandae quo neque, repudiandae reprehenderit consequuntur
+          eum suscipit, temporibus, voluptate excepturi quod soluta eveniet.
+          Perferendis quis repellat officiis cum.
+        </p>
+      </div>
+    </Layout>
+  )
+};
+
+export default About;
